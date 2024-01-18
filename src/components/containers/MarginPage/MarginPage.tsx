227149a43999004d305d6521c31ddf29b6f6758d
José Rojas <joseantoniorojasl02@gmail.com>
2024-01-18 18:36:45 -0400

    Fix Class Tailwind

--- src/components/containers/MarginPage/MarginPage.tsx
+++ src/components/containers/MarginPage/MarginPage.tsx
@@ -6,7 +6,7 @@ interface Props {
 export const MarginPage = ({ className, children }: Props) => {
   return (
     <div
-      className={`w-[90%] m-auto wx-[1500px] ${
+      className={`w-[90%] m-auto max-w-[1500px] ${
         className === undefined ? '' : className
       }`}
     >
