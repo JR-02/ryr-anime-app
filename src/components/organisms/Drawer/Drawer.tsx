7df51bef596eb58ebc997471c562d922e68a6bdc
José Rojas <joseantoniorojasl02@gmail.com>
2024-01-18 18:53:42 -0400

    ⬛ Add screen black component Drawer

--- src/components/organisms/Drawer/Drawer.tsx
+++ src/components/organisms/Drawer/Drawer.tsx
@@ -30,33 +30,41 @@ export const Drawer = ({ showDrawer, setShowDrawer }: DrawerProps) => {
   ];
 
   return (
-    <section
-      className={`fixed z-50 bg-bg100 w-[250px] h-screen ${
-        showDrawer ? 'right-0' : '-right-[250px]'
-      } top-0 p-4 duration-200`}
-    >
-      <div className='w-full bg-accent100 rounded p-1 mb-3 flex items-center'>
-        <button
+    <section>
+      {showDrawer && (
+        <div
           onClick={handleShowDrawer}
-          className='hover:bg-bg300/40 p-1 rounded-full'
-        >
-          <MenuIcon width={40} height={40} className='fill-tx100' />
-        </button>
-        <span className='uppercase text-xl font-semibold text-tx100 text-center flex-grow'>
-          R&R:Anime
-        </span>
-      </div>
+          className='fixed bg-[#000]/70 inset-0 z-40 h-screen'
+        />
+      )}
+      <section
+        className={`flex flex-col gap-4 fixed z-50 bg-bg100 w-[250px] h-screen ${
+          showDrawer ? 'right-0' : '-right-[250px]'
+        } top-0 p-4 duration-200`}
+      >
+        <div className='w-full bg-accent100 rounded p-1 flex items-center'>
+          <button
+            onClick={handleShowDrawer}
+            className='hover:bg-bg300/40 p-1 rounded-full'
+          >
+            <MenuIcon width={40} height={40} className='fill-tx100' />
+          </button>
+          <span className='uppercase text-xl font-semibold text-tx100 text-center flex-grow'>
+            R&R:Anime
+          </span>
+        </div>
 
-      <SearchHeader className='w-full bg-bg200 lg:hidden' />
+        <SearchHeader className='w-full bg-bg200 lg:hidden' />
 
-      <nav className='w-full'>
-        {links.map(({ title, icon, href }) => (
-          <LinkNavbar href={href} key={title}>
-            {icon}
-            {title}
-          </LinkNavbar>
-        ))}
-      </nav>
+        <nav className='w-full'>
+          {links.map(({ title, icon, href }) => (
+            <LinkNavbar href={href} key={title}>
+              {icon}
+              {title}
+            </LinkNavbar>
+          ))}
+        </nav>
+      </section>
     </section>
   );
 };
